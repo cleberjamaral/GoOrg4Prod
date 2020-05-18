d65bb7669607f4b47dc6bc15a26606bcea097cbc
cleberjamaral <cleberjamaral@gmail.com>
2020-05-18 06:51:50 -0300

    Added stub for FirstFitDecreasing

---
+++ src/main/java/fit/FirstFitDecreasing.java
@@ -0,0 +1,10 @@
+package fit;
+
+import java.util.Set;
+
+public class FirstFitDecreasing extends Fit {
+
+//	@Override
+//	public void fitRequirements(Set<Requirement> requirements, Set<Resource> resources) {
+//	}
+}
