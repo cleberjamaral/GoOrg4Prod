9fcc221cfb5d8f92be93cf9b1bc630c2b45921f9
cleberjamaral <cleberjamaral@gmail.com>
2020-05-26 22:33:33 -0300

    Simplifying methods for testing exceptions

--- src/test/java/organisation/exception/OrganisationException.java
+++ src/test/java/organisation/exception/OrganisationException.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import busca.BuscaLargura;
 import busca.Nodo;
+import organisation.OrganisationStatistics;
 import organisation.goal.GoalNode;
 import organisation.goal.GoalTree;
 import organisation.search.Organisation;
@@ -18,61 +19,55 @@ public class OrganisationException {
 	public void testOutputDoesNotMatchWithInput() {
 		System.out.println("\n\ntestOutputDoesNotMatchWithInput");
 
-		GoalNode g0 = new GoalNode(null, "g0");
-		GoalTree gTree = GoalTree.getInstance();
-		gTree.setRootNode(g0);
 		try {
+			GoalNode g0 = new GoalNode(null, "g0");
+			GoalTree gTree = GoalTree.getInstance();
+			gTree.setRootNode(g0);
 			gTree.addWorkload("g0", "w0", 1);
-		} catch (Exception ex) {
-			System.out.println("Exception catched: " + ex.getMessage());
-		}
 
-		System.out.println("GoalsTree: " + gTree.getTree().toString());
-		Organisation o = new Organisation("testOutputDoesNotMatchWithInput", gTree, Cost.GENERALIST, true);
-		Nodo n = new BuscaLargura().busca(o);
+			System.out.println("GoalsTree: " + gTree.getTree().toString());
 
-		System.out.println("Validate original gdt workloads with organisations' workloads (must be ok)");
-		try {
-			assertTrue(((Organisation) n.getEstado()).isValid());
-		} catch (Exception ex) {
-			System.out.println("Exception catched: " + ex.getMessage());
-		}
+			OrganisationStatistics s = OrganisationStatistics.getInstance();
+			s.prepareGenerationStatisticsFile("testOutputDoesNotMatchWithInput");
 
-		System.out.println("Force a wrong validation, which must throw an exception");
+			Organisation o = new Organisation("testOutputDoesNotMatchWithInput", gTree, Cost.GENERALIST, true);
+			Nodo n = new BuscaLargura().busca(o);
 
-		try {
+			System.out.println("Force a wrong validation, which must throw an exception");
+
 			gTree.addWorkload("g0", "w1", 1);
-		} catch (Exception ex) {
-			System.out.println("Exception catched: " + ex.getMessage());
+			assertThrows(OutputDoesNotMatchWithInput.class, () -> {
+				((Organisation) n.getEstado()).isValid();
+			});
+			System.out.println("Exception thrown successfully!");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		assertThrows(OutputDoesNotMatchWithInput.class, () -> {
-			((Organisation) n.getEstado()).isValid();
-		});
 	}
 
 	@Test
 	public void testPositionNotFound() {
 		System.out.println("\n\ntestPositionNotFound");
 
-		GoalNode g0 = new GoalNode(null, "g0");
-		GoalTree gTree = GoalTree.getInstance();
-		gTree.setRootNode(g0);
 		try {
+			GoalNode g0 = new GoalNode(null, "g0");
+			GoalTree gTree = GoalTree.getInstance();
+			gTree.setRootNode(g0);
 			gTree.addWorkload("g0", "w0", 1);
-		} catch (Throwable ex) {
-			System.out.println("Exception: " + ex.getMessage());
+
+			System.out.println("GoalsTree: " + gTree.getTree().toString());
+			Organisation o = new Organisation("testPositionNotFound", gTree, Cost.GENERALIST, true);
+			Nodo n = new BuscaLargura().busca(o);
+
+			System.out.println("Positions tree: " + ((Organisation) n.getEstado()).getPositionsTree());
+			System.out.println("search for a nonexistant positionname");
+			assertThrows(PositionNotFound.class, () -> {
+				((Organisation) n.getEstado()).getPositionsTree().findPositionByName("nonexistant");
+			});
+			System.out.println("Exception thrown successfully!");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-
-		System.out.println("GoalsTree: " + gTree.getTree().toString());
-		Organisation o = new Organisation("testPositionNotFound", gTree, Cost.GENERALIST, true);
-		Nodo n = new BuscaLargura().busca(o);
-
-		System.out.println("Positions tree: " + ((Organisation) n.getEstado()).getPositionsTree());
-		System.out.println("search for a nonexistant positionname");
-		assertThrows(PositionNotFound.class, () -> {
-			((Organisation) n.getEstado()).getPositionsTree().findPositionByName("nonexistant");
-		});
-
 	}
 
 }
